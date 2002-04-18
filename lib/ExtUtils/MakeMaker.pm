45684af1ce5b4b6582f8d7e0ff53269a77470f04
Michael G Schwern <schwern@pobox.com>
2002-04-18 17:14:19 +0000

    Incrementing version
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@684 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MakeMaker.pm
+++ lib/ExtUtils/MakeMaker.pm
@@ -2,10 +2,10 @@ BEGIN {require 5.004;}
 
 package ExtUtils::MakeMaker;
 
-$VERSION = "5.90_01";
+$VERSION = "5.91_01";
 $Version_OK = "5.49";   # Makefiles older than $Version_OK will die
                         # (Will be checked from MakeMaker version 4.13 onwards)
-($Revision = substr(q$Revision: 1.38 $, 10)) =~ s/\s+$//;
+($Revision = substr(q$Revision: 1.39 $, 10)) =~ s/\s+$//;
 
 require Exporter;
 use Config;
@@ -1804,7 +1804,7 @@ MakeMaker object. The following lines will be parsed o.k.:
 
     $VERSION = '1.00';
     *VERSION = \'1.01';
-    ( $VERSION ) = '$Revision: 1.38 $ ' =~ /\$Revision:\s+([^\s]+)/;
+    ( $VERSION ) = '$Revision: 1.39 $ ' =~ /\$Revision:\s+([^\s]+)/;
     $FOO::VERSION = '1.10';
     *FOO::VERSION = \'1.11';
     our $VERSION = 1.2.3;       # new for perl5.6.0 
