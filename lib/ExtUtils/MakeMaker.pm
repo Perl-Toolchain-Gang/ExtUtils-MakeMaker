a2765022c20f4c1fe11707f19c93c70a8f667eeb
Michael G Schwern <schwern@pobox.com>
2002-04-22 04:45:09 +0000

    Documenting VERBINST
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@704 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MakeMaker.pm
+++ lib/ExtUtils/MakeMaker.pm
@@ -5,7 +5,7 @@ package ExtUtils::MakeMaker;
 $VERSION = "5.91_01";
 $Version_OK = "5.49";   # Makefiles older than $Version_OK will die
                         # (Will be checked from MakeMaker version 4.13 onwards)
-($Revision = substr(q$Revision: 1.41 $, 10)) =~ s/\s+$//;
+($Revision = substr(q$Revision: 1.42 $, 10)) =~ s/\s+$//;
 
 require Exporter;
 use Config;
@@ -1782,6 +1782,10 @@ Like PREFIX, but only for the vendor install locations.
 
 Defaults to PREFIX (if set) or $Config{vendorprefixexp}
 
+=item VERBINST
+
+If true, make install will be verbose
+
 =item VERSION
 
 Your version number for distributing the package.  This defaults to
@@ -1803,7 +1807,7 @@ MakeMaker object. The following lines will be parsed o.k.:
 
     $VERSION = '1.00';
     *VERSION = \'1.01';
-    ( $VERSION ) = '$Revision: 1.41 $ ' =~ /\$Revision:\s+([^\s]+)/;
+    ( $VERSION ) = '$Revision: 1.42 $ ' =~ /\$Revision:\s+([^\s]+)/;
     $FOO::VERSION = '1.10';
     *FOO::VERSION = \'1.11';
     our $VERSION = 1.2.3;       # new for perl5.6.0 
