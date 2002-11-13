72c07f8ce88f0e89c9bc75033ab1d404dbcfed96
Michael G Schwern <schwern@pobox.com>
2002-11-13 11:17:14 +0000

    Turns out all this manifypods override was doing was changing the man page name style from Foo::Bar.3 to Foo.Bar.3.  This is better done by overriding init_dirscan().
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@866 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MM_Cygwin.pm
+++ lib/ExtUtils/MM_Cygwin.pm
@@ -10,7 +10,7 @@ require ExtUtils::MM_Any;
 require ExtUtils::MM_Unix;
 @ISA = qw( ExtUtils::MM_Any ExtUtils::MM_Unix );
 
-$VERSION = 1.04;
+$VERSION = 1.05;
 
 sub cflags {
     my($self,$libperl)=@_;
@@ -31,49 +31,18 @@ PERLTYPE = $self->{PERLTYPE}
 
 }
 
-sub manifypods {
-    my($self, %attribs) = @_;
-    return "\nmanifypods : pure_all\n\t$self->{NOECHO}\$(NOOP)\n" unless
-        %{$self->{MAN3PODS}} or %{$self->{MAN1PODS}};
-    my($dist);
-    my($pod2man_exe);
-    if (defined $self->{PERL_SRC}) {
-        $pod2man_exe = $self->catfile($self->{PERL_SRC},'pod','pod2man');
-    } else {
-        $pod2man_exe = $self->catfile($Config{scriptdirexp},'pod2man');
-    }
-    unless ($self->perl_script($pod2man_exe)) {
-        # No pod2man but some MAN3PODS to be installed
-        print <<END;
 
-Warning: I could not locate your pod2man program. Please make sure,
-         your pod2man program is in your PATH before you execute 'make'
+# Override to change man page name styles from Foo::Bar.N Unix style to
+# Foo.Bar.N Cygwin style.
+sub init_dirscan {
+    my($self) = shift;
+    $self->SUPER::init_dirscan;
 
-END
-        $pod2man_exe = "-S pod2man";
+    foreach my $manpage (values %{$self->{MAN1PODS}}, 
+                         values %{$self->{MAN3PODS}})
+    {
+        $manpage =~ s/::/./g;
     }
-    my(@m) = ();
-    push @m,
-qq[POD2MAN_EXE = $pod2man_exe\n],
-qq[POD2MAN = \$(PERL) -we '%m=\@ARGV;for (keys %m){' \\\n],
-q[-e 'next if -e $$m{$$_} && -M $$m{$$_} < -M $$_ && -M $$m{$$_} < -M "],
- $self->{MAKEFILE}, q[";' \\
--e 'print "Manifying $$m{$$_}\n"; $$m{$$_} =~ s/::/./g;' \\
--e 'system(qq[$(PERLRUN) $(POD2MAN_EXE) ].qq[$$_>$$m{$$_}])==0 or warn "Couldn\\047t install $$m{$$_}\n";' \\
--e 'chmod(oct($(PERM_RW))), $$m{$$_} or warn "chmod $(PERM_RW) $$m{$$_}: $$!\n";}'
-];
-    push @m, "\nmanifypods : pure_all ";
-    push @m, join " \\\n\t", keys %{$self->{MAN1PODS}},
-                             keys %{$self->{MAN3PODS}};
-
-    push(@m,"\n");
-    if (%{$self->{MAN1PODS}} || %{$self->{MAN3PODS}}) {
-        grep { $self->{MAN1PODS}{$_} =~ s/::/./g } keys %{$self->{MAN1PODS}};
-        grep { $self->{MAN3PODS}{$_} =~ s/::/./g } keys %{$self->{MAN3PODS}};
-        push @m, "\t$self->{NOECHO}\$(POD2MAN) \\\n\t";
-        push @m, join " \\\n\t", %{$self->{MAN1PODS}}, %{$self->{MAN3PODS}};
-    }
-    join('', @m);
 }
 
 sub perl_archive {
@@ -105,17 +74,13 @@ See ExtUtils::MM_Unix for a documentation of the methods provided there.
 
 =over 4
 
-=item canonpath
-
-replaces backslashes with forward ones.  then acts as *nixish.
-
 =item cflags
 
 if configured for dynamic loading, triggers #define EXT in EXTERN.h
 
-=item manifypods
+=item init_dirscan
 
-replaces strings '::' with '.' in man page names
+replaces strings '::' with '.' in MAN*POD man page names
 
 =item perl_archive
 
