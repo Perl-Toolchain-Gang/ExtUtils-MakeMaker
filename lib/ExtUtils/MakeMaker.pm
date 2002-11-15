c33a7ccafcadc62b9c2bec94334da212c48dd9cb
Michael G Schwern <schwern@pobox.com>
2002-11-15 07:40:42 +0000

    * Added a MAKEFILE macro to the Makefile.  Mostly to avoid having       to hardcode $self->{MAKEFILE} internally
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@921 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MakeMaker.pm
+++ lib/ExtUtils/MakeMaker.pm
@@ -5,7 +5,7 @@ BEGIN {require 5.005_03;}
 $VERSION = "6.05";
 $Version_OK = "5.49";   # Makefiles older than $Version_OK will die
                         # (Will be checked from MakeMaker version 4.13 onwards)
-($Revision = substr(q$Revision: 1.71 $, 10)) =~ s/\s+$//;
+($Revision = substr(q$Revision: 1.72 $, 10)) =~ s/\s+$//;
 
 require Exporter;
 use Config;
@@ -191,7 +191,7 @@ sub full_setup {
 
     AUTHOR ABSTRACT ABSTRACT_FROM BINARY_LOCATION
     C CAPI CCFLAGS CONFIG CONFIGURE DEFINE DIR DISTNAME DL_FUNCS DL_VARS
-    EXCLUDE_EXT EXE_FILES FIRST_MAKEFILE 
+    EXCLUDE_EXT EXE_FILES FIRST_MAKEFILE
     FULLPERL FULLPERLRUN FULLPERLRUNINST
     FUNCLIST H IMPORTS
     INST_ARCHLIB INST_SCRIPT INST_BIN INST_LIB INST_MAN1DIR INST_MAN3DIR
@@ -1912,7 +1912,7 @@ MakeMaker object. The following lines will be parsed o.k.:
 
     $VERSION = '1.00';
     *VERSION = \'1.01';
-    ( $VERSION ) = '$Revision: 1.71 $ ' =~ /\$Revision:\s+([^\s]+)/;
+    ( $VERSION ) = '$Revision: 1.72 $ ' =~ /\$Revision:\s+([^\s]+)/;
     $FOO::VERSION = '1.10';
     *FOO::VERSION = \'1.11';
     our $VERSION = 1.2.3;       # new for perl5.6.0 
