672458c2936e448e823a8efa75d46838a74dd514
Michael G Schwern <schwern@pobox.com>
2003-07-31 23:46:57 +0000

    Whoops.  Not stomping on WriteMakefile() args logic was backwards.
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@1658 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MakeMaker.pm
+++ lib/ExtUtils/MakeMaker.pm
@@ -3,7 +3,7 @@ package ExtUtils::MakeMaker;
 BEGIN {require 5.005_03;}
 
 $VERSION = '6.13';
-($Revision) = q$Revision: 1.126 $ =~ /Revision:\s+(\S+)/;
+($Revision) = q$Revision: 1.127 $ =~ /Revision:\s+(\S+)/;
 
 require Exporter;
 use Config;
@@ -439,8 +439,8 @@ sub new {
             next unless defined $self->{PARENT}{$key};
 
             # Don't stomp on WriteMakefile() args.
-            next unless defined $self->{ARGS}{$key} and
-                        $self->{ARGS}{$key} eq $self->{$key};
+            next if defined $self->{ARGS}{$key} and
+                    $self->{ARGS}{$key} eq $self->{$key};
 
             $self->{$key} = $self->{PARENT}{$key};
 
@@ -2038,7 +2038,7 @@ MakeMaker object. The following lines will be parsed o.k.:
 
     $VERSION = '1.00';
     *VERSION = \'1.01';
-    $VERSION = sprintf "%d.%03d", q$Revision: 1.126 $ =~ /(\d+)/g;
+    $VERSION = sprintf "%d.%03d", q$Revision: 1.127 $ =~ /(\d+)/g;
     $FOO::VERSION = '1.10';
     *FOO::VERSION = \'1.11';
     our $VERSION = 1.2.3;       # new for perl5.6.0 
