74959de6b52bdbee22fd581002bfec85ed0e9f64
Michael G Schwern <schwern@pobox.com>
2003-10-30 23:36:12 +0000

    Version up
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@1765 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/Manifest.pm
+++ lib/ExtUtils/Manifest.pm
@@ -12,7 +12,7 @@ use vars qw($VERSION @ISA @EXPORT_OK
           $Is_MacOS $Is_VMS 
           $Debug $Verbose $Quiet $MANIFEST $DEFAULT_MSKIP);
 
-$VERSION = 1.42;
+$VERSION = 1.43;
 @ISA=('Exporter');
 @EXPORT_OK = qw(mkmanifest
                 manicheck  filecheck  fullcheck  skipcheck
