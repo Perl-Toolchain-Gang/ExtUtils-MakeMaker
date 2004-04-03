27171ba285422d3ac869ea22d342ead9e44e4e2c
Michael G Schwern <schwern@pobox.com>
2004-04-03 00:38:07 +0000

    - ensure ExtUtils::Command::rm_f deletes all versions of a file on VMS       [rt 4687]
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@1951 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/Command.pm
+++ lib/ExtUtils/Command.pm
@@ -103,18 +103,32 @@ Removes files (even if readonly)
 
 =cut 
 
-sub rm_f
-{
- expand_wildcards();
- foreach (@ARGV)
-  {
-   next unless -f $_;
-   next if unlink($_);
-   chmod(0777,$_);
-   next if unlink($_);
-   carp "Cannot delete $_:$!";
-  }
+sub rm_f {
+    expand_wildcards();
+
+    foreach my $file (@ARGV) {
+        next unless -f $file;
+
+        next if _unlink($file);
+
+        chmod(0777, $file);
+
+        next if _unlink($file);
+            
+        carp "Cannot delete $file: $!";
+    }
 }
+
+sub _unlink {
+    my $files_unlinked = 0;
+    foreach my $file (@_) {
+        my $delete_count = 0;
+        $delete_count++ while unlink $file;
+        $files_unlinked++ if $delete_count;
+    }
+    return $files_unlinked;
+}
+
 
 =item touch files ...
 
