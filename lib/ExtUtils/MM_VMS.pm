286ba81d889a37b90457f6ba545d118d04e254c1
Michael G Schwern <schwern@pobox.com>
2006-09-01 05:05:07 +0000

    r17720@windhund:  schwern | 2006-09-01 01:00:05 -0400      - VMS->one_liner must quote "--" argument. [bleadperl@27613]
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@3754 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MM_VMS.pm
+++ lib/ExtUtils/MM_VMS.pm
@@ -1722,7 +1722,7 @@ sub oneliner {
     # Switches must be quoted else they will be lowercased.
     $switches = join ' ', map { qq{"$_"} } @$switches;
 
-    return qq{\$(ABSPERLRUN) $switches -e $cmd --};
+    return qq{\$(ABSPERLRUN) $switches -e $cmd "--"};
 }
 
 
