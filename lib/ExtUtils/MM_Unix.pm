ae1c1f194db081f1bd4909a8499c5750a479d142
Michael G Schwern <schwern@pobox.com>
2006-09-11 21:38:02 +0000

    r18035@Master-Windhund-IV:  schwern | 2006-09-11 16:18:19 -0400  6.30_04
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@3818 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MM_Unix.pm
+++ lib/ExtUtils/MM_Unix.pm
@@ -18,7 +18,7 @@ use vars qw($VERSION @ISA
 
 use ExtUtils::MakeMaker qw($Verbose neatvalue);
 
-$VERSION = '1.50_02';
+$VERSION = '1.50_04';
 
 require ExtUtils::MM_Any;
 @ISA = qw(ExtUtils::MM_Any);
