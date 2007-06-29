a58c5e35f5e50ae081c5411737573052e9495721
David H. Adler <dha@panix.com>
2007-06-29 02:09:47 +0000

    tidied up fixin()
    git-svn-id: http://svn.schwern.org/repos/CPAN/ExtUtils-MakeMaker/trunk@3967 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/ExtUtils/MM_Unix.pm
+++ lib/ExtUtils/MM_Unix.pm
@@ -1066,95 +1066,104 @@ Inserts the sharpbang or equivalent magic number to a set of @files.
 
 =cut
 
-sub fixin { # stolen from the pink Camel book, more or less
-    my($self, @files) = @_;
+sub fixin {    # stolen from the pink Camel book, more or less
+    my ( $self, @files ) = @_;
 
-    my($does_shbang) = $Config{'sharpbang'} =~ /^\s*\#\!/;
+    my ($does_shbang) = $Config{'sharpbang'} =~ /^\s*\#\!/;
     for my $file (@files) {
         my $file_new = "$file.new";
         my $file_bak = "$file.bak";
 
-	local(*FIXIN);
-	local(*FIXOUT);
-	open(FIXIN, $file) or croak "Can't process '$file': $!";
-	local $/ = "\n";
-	chomp(my $line = <FIXIN>);
-	next unless $line =~ s/^\s*\#!\s*//;     # Not a shbang file.
-	# Now figure out the interpreter name.
-	my($cmd,$arg) = split ' ', $line, 2;
-	$cmd =~ s!^.*/!!;
+        local (*FIXIN);
+        local (*FIXOUT);
+        open( FIXIN, $file ) or croak "Can't process '$file': $!";
+        local $/ = "\n";
+        chomp( my $line = <FIXIN> );
+        next unless $line =~ s/^\s*\#!\s*//;    # Not a shbang file.
+        # Now figure out the interpreter name.
+        my ( $cmd, $arg ) = split ' ', $line, 2;
+        $cmd =~ s!^.*/!!;
 
-	# Now look (in reverse) for interpreter in absolute PATH (unless perl).
+        # Now look (in reverse) for interpreter in absolute PATH (unless perl).
         my $interpreter;
-	if ($cmd eq "perl") {
-            if ($Config{startperl} =~ m,^\#!.*/perl,) {
+        if ( $cmd eq "perl" ) {
+            if ( $Config{startperl} =~ m,^\#!.*/perl, ) {
                 $interpreter = $Config{startperl};
                 $interpreter =~ s,^\#!,,;
-            } else {
+            }
+            else {
                 $interpreter = $Config{perlpath};
             }
-	} else {
-	    my(@absdirs) = reverse grep {$self->file_name_is_absolute} $self->path;
-	    $interpreter = '';
-	    my($dir);
-	    foreach $dir (@absdirs) {
-		if ($self->maybe_command($cmd)) {
-		    warn "Ignoring $interpreter in $file\n" if $Verbose && $interpreter;
-		    $interpreter = $self->catfile($dir,$cmd);
-		}
-	    }
-	}
-	# Figure out how to invoke interpreter on this machine.
+        }
+        else {
+            my (@absdirs)
+                = reverse grep { $self->file_name_is_absolute } $self->path;
+            $interpreter = '';
+            my ($dir);
+            foreach $dir (@absdirs) {
+                if ( $self->maybe_command($cmd) ) {
+                    warn "Ignoring $interpreter in $file\n"
+                        if $Verbose && $interpreter;
+                    $interpreter = $self->catfile( $dir, $cmd );
+                }
+            }
+        }
 
-	my($shb) = "";
-	if ($interpreter) {
-	    print STDOUT "Changing sharpbang in $file to $interpreter" if $Verbose;
-	    # this is probably value-free on DOSISH platforms
-	    if ($does_shbang) {
-		$shb .= "$Config{'sharpbang'}$interpreter";
-		$shb .= ' ' . $arg if defined $arg;
-		$shb .= "\n";
-	    }
-	    $shb .= qq{
+        # Figure out how to invoke interpreter on this machine.
+
+        my ($shb) = "";
+        if ($interpreter) {
+            print STDOUT "Changing sharpbang in $file to $interpreter"
+                if $Verbose;
+
+            # this is probably value-free on DOSISH platforms
+            if ($does_shbang) {
+                $shb .= "$Config{'sharpbang'}$interpreter";
+                $shb .= ' ' . $arg if defined $arg;
+                $shb .= "\n";
+            }
+            $shb .= qq{
 eval 'exec $interpreter $arg -S \$0 \${1+"\$\@"}'
     if 0; # not running under some shell
-} unless $Is_Win32; # this won't work on win32, so don't
-	} else {
-	    warn "Can't find $cmd in PATH, $file unchanged"
-		if $Verbose;
-	    next;
-	}
+} unless $Is_Win32;    # this won't work on win32, so don't
+        }
+        else {
+            warn "Can't find $cmd in PATH, $file unchanged"
+                if $Verbose;
+            next;
+        }
 
-	unless ( open(FIXOUT,">$file_new") ) {
-	    warn "Can't create new $file: $!\n";
-	    next;
-	}
-	
-	# Print out the new #! line (or equivalent).
-	local $\;
-	local $/;
-	print FIXOUT $shb, <FIXIN>;
-	close FIXIN;
-	close FIXOUT;
+        unless ( open( FIXOUT, ">$file_new" ) ) {
+            warn "Can't create new $file: $!\n";
+            next;
+        }
+
+        # Print out the new #! line (or equivalent).
+        local $\;
+        local $/;
+        print FIXOUT $shb, <FIXIN>;
+        close FIXIN;
+        close FIXOUT;
 
         chmod 0666, $file_bak;
         unlink $file_bak;
-	unless ( _rename($file, $file_bak) ) {	
-	    warn "Can't rename $file to $file_bak: $!";
-	    next;
-	}
-	unless ( _rename($file_new, $file) ) {	
-	    warn "Can't rename $file_new to $file: $!";
-	    unless ( _rename($file_bak, $file) ) {
-	        warn "Can't rename $file_bak back to $file either: $!";
-		warn "Leaving $file renamed as $file_bak\n";
-	    }
-	    next;
-	}
-	unlink $file_bak;
-    } continue {
-	close(FIXIN) if fileno(FIXIN);
-	system("$Config{'eunicefix'} $file") if $Config{'eunicefix'} ne ':';;
+        unless ( _rename( $file, $file_bak ) ) {
+            warn "Can't rename $file to $file_bak: $!";
+            next;
+        }
+        unless ( _rename( $file_new, $file ) ) {
+            warn "Can't rename $file_new to $file: $!";
+            unless ( _rename( $file_bak, $file ) ) {
+                warn "Can't rename $file_bak back to $file either: $!";
+                warn "Leaving $file renamed as $file_bak\n";
+            }
+            next;
+        }
+        unlink $file_bak;
+    }
+    continue {
+        close(FIXIN) if fileno(FIXIN);
+        system("$Config{'eunicefix'} $file") if $Config{'eunicefix'} ne ':';
     }
 }
 
