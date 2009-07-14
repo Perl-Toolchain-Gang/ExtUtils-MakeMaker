a129fd8190b4ef304710ce072a13360153c377e7
Michael G. Schwern <schwern@pobox.com>
2009-07-14 16:01:45 -0700

    Version 6.55_01

--- lib/ExtUtils/MM_Any.pm
+++ lib/ExtUtils/MM_Any.pm
@@ -1,7 +1,7 @@
 package ExtUtils::MM_Any;
 
 use strict;
-our $VERSION = '6.54';
+our $VERSION = '6.55_01';
 
 use Carp;
 use File::Spec;
--- lib/ExtUtils/MakeMaker.pm
+++ lib/ExtUtils/MakeMaker.pm
@@ -18,7 +18,7 @@ our @Overridable;
 my @Prepend_parent;
 my %Recognized_Att_Keys;
 
-our $VERSION = '6.54';
+our $VERSION = '6.55_01';
 
 # Emulate something resembling CVS $Revision$
 (our $Revision = $VERSION) =~ s{_}{};
