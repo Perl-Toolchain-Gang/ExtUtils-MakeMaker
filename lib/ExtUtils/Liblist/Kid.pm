cd0d391c2d3b94855260e374bdf804100c63a56b
Michael G. Schwern <schwern@pobox.com>
2011-07-06 14:22:01 -0700

    Version 6.58

--- lib/ExtUtils/Liblist/Kid.pm
+++ lib/ExtUtils/Liblist/Kid.pm
@@ -11,7 +11,7 @@ use 5.006;
 
 use strict;
 use warnings;
-our $VERSION = '6.57_11';
+our $VERSION = '6.58';
 
 use ExtUtils::MakeMaker::Config;
 use Cwd 'cwd';
--- lib/ExtUtils/MM_Any.pm
+++ lib/ExtUtils/MM_Any.pm
@@ -1,7 +1,7 @@
 package ExtUtils::MM_Any;
 
 use strict;
-our $VERSION = '6.57_11';
+our $VERSION = '6.58';
 
 use Carp;
 use File::Spec;
