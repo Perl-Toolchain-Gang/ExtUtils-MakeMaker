653aed78aa33dadd9acb47968c507b8ada3a69bd
Michael G. Schwern <schwern@pobox.com>
2012-11-01 20:58:02 -0700

    Version 6.63_03

--- lib/ExtUtils/MakeMaker.pm
+++ lib/ExtUtils/MakeMaker.pm
@@ -18,7 +18,7 @@ our @Overridable;
 my @Prepend_parent;
 my %Recognized_Att_Keys;
 
-our $VERSION = '6.63_02';
+our $VERSION = '6.63_03';
 $VERSION = eval $VERSION;
 
 # Emulate something resembling CVS $Revision$
