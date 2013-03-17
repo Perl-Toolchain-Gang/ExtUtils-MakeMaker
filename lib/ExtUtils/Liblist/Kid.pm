a1bf24331e92ce7eae98ce9a0078855ae43ae6b4
Yves Orton <demerphq@gmail.com>
2013-03-17 23:53:56 +0100

    Bump versions, update changes, and fixup manifest for 6.65_01

--- lib/ExtUtils/Liblist/Kid.pm
+++ lib/ExtUtils/Liblist/Kid.pm
@@ -11,7 +11,7 @@ use 5.006;
 
 use strict;
 use warnings;
-our $VERSION = '6.64';
+our $VERSION = '6.65_01';
 
 use ExtUtils::MakeMaker::Config;
 use Cwd 'cwd';
--- lib/ExtUtils/MM_Any.pm
+++ lib/ExtUtils/MM_Any.pm
@@ -1,7 +1,7 @@
 package ExtUtils::MM_Any;
 
 use strict;
-our $VERSION = '6.64';
+our $VERSION = '6.65_01';
 
 use Carp;
 use File::Spec;
