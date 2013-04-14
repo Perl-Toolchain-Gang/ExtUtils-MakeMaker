bc5a3322ee5bc86800a923b45502b69233b468d9
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-04-14 10:59:53 +0100

    Bump versions, update changes, and fixup manifest for 6.65_02

--- lib/ExtUtils/Liblist.pm
+++ lib/ExtUtils/Liblist.pm
@@ -2,7 +2,7 @@ package ExtUtils::Liblist;
 
 use strict;
 
-our $VERSION = '6.65_01';
+our $VERSION = '6.65_02';
 
 use File::Spec;
 require ExtUtils::Liblist::Kid;
--- lib/ExtUtils/MakeMaker/Config.pm
+++ lib/ExtUtils/MakeMaker/Config.pm
@@ -2,7 +2,7 @@ package ExtUtils::MakeMaker::Config;
 
 use strict;
 
-our $VERSION = '6.65_01';
+our $VERSION = '6.65_02';
 
 use Config ();
 
