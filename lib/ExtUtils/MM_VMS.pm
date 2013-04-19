ce78d0f7b64c396929460219979619dfd15fbb8c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-04-19 18:52:36 +0100

    Update Changes and bump version to 6.66

--- lib/ExtUtils/MM_VMS.pm
+++ lib/ExtUtils/MM_VMS.pm
@@ -15,7 +15,7 @@ BEGIN {
 
 use File::Basename;
 
-our $VERSION = '6.65_03';
+our $VERSION = '6.66';
 
 require ExtUtils::MM_Any;
 require ExtUtils::MM_Unix;
--- lib/ExtUtils/MakeMaker/Config.pm
+++ lib/ExtUtils/MakeMaker/Config.pm
@@ -2,7 +2,7 @@ package ExtUtils::MakeMaker::Config;
 
 use strict;
 
-our $VERSION = '6.65_03';
+our $VERSION = '6.66';
 
 use Config ();
 
