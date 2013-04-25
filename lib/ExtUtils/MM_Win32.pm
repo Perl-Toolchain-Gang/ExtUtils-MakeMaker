5a884a64ae9dccedbbfc432928b394161d20c44a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-04-25 19:18:46 +0100

    RT#77215 Increase MAXLINELENGTH to 80000
    MM_Win32.pm sets by default MAXLINELENGTH (dmake related) to 64 * 1024
    which is not enough e.g. for installing Date::Manip
    
    The trouble is that the way MM_Win32.pm sets this value overrides all
    dmake config files - for example in strawberry perl dmake is preconfigured
    with MAXLINELENGTH := 800000 however MM_Win32.pm takes it down to 65536
    
    Not sure what is the correct way to handle this. I would at least prefer
    to increase the default value - my proposal is 800000
    
    Rpeorted by kmx

--- lib/ExtUtils/MM_Win32.pm
+++ lib/ExtUtils/MM_Win32.pm
@@ -251,7 +251,7 @@ sub constants {
     #
     # This has to come here before all the constants and not in
     # platform_constants which is after constants.
-    my $size = $self->{MAXLINELENGTH} || 64 * 1024;
+    my $size = $self->{MAXLINELENGTH} || 800000;
     my $prefix = qq{
 # Get dmake to read long commands like PM_TO_BLIB
 MAXLINELENGTH = $size
