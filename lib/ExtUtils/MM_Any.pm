f0457fc944c1ba0658b52af88866fba10fac505a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-07-02 12:24:32 +0100

    Handle v-strings when they are specified as VERSION in WriteMakefile()

--- lib/ExtUtils/MM_Any.pm
+++ lib/ExtUtils/MM_Any.pm
@@ -1987,6 +1987,11 @@ sub init_VERSION {
     if (defined $self->{VERSION}) {
         $self->{VERSION} =~ s/^\s+//;
         $self->{VERSION} =~ s/\s+$//;
+        if ( $self->{VERSION} !~ /^v?[\d_\.]+$/ ) {
+          require version;
+          my $normal = eval { version->parse( $self->{VERSION} ) };
+          $self->{VERSION} = $normal if defined $normal;
+        }
     }
     else {
         $self->{VERSION} = '';
