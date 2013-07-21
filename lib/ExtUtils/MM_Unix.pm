66af21da6293e9adc29c64b5c63af8a569da602c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-07-21 09:20:55 +0100

    [RT#86976] Version parsing bug introduced in 6.69_05 Part Deux
    More regression redux

--- lib/ExtUtils/MM_Unix.pm
+++ lib/ExtUtils/MM_Unix.pm
@@ -2645,6 +2645,7 @@ sub parse_version {
         next if $inpod || /^\s*#/;
         chop;
         next if /^\s*(if|unless|elsif)/;
+        s{(?<!\\)\#.+?$}{}g;
         if ( m{^ \s* package \s+ \w[\w\:\']* \s+ (v?[0-9._]+) \s* ;  }x ) {
             local $^W = 0;
             $result = $1;
