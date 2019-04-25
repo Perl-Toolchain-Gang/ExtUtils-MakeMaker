2b4bdbe7991f4b6e940397bf3b543b7af2ff044f
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2019-04-25 11:52:53 +0100

    RT#128004 gcc AIX builds broken

--- lib/ExtUtils/MM_AIX.pm
+++ lib/ExtUtils/MM_AIX.pm
@@ -50,7 +50,9 @@ sub xs_dlsyms_ext {
 
 sub xs_dlsyms_arg {
     my($self, $file) = @_;
-    return qq{-bE:${file}};
+    my $arg = qq{-bE:${file}};
+    $arg = '-Wl,'.$arg if $Config{lddlflags} =~ /-Wl,-bE:/;
+    return $arg;
 }
 
 sub init_others {
