Perl-Toolchain-Gang/ExtUtils-MakeMaker

commit a07d951129587be9c6246e967afc754f4b0cbd68

Workaround for RT#133493
Apple have started to apply -Werror by default breaking lots of XS modules

Signed-off-by: Chris 'BinGOs' Williams <chris@bingosnet.co.uk>

lib/ExtUtils/MM_Darwin.pm
@@ -46,4 +46,18 @@ sub init_dist {
     $self->SUPER::init_dist(@_);
 }
 
+=head3 cflags
+
+Over-ride Apple's automatic setting of -Werror
+
+=cut
+
+sub cflags {
+    my $self = shift;
+
+    $self->{CCFLAGS} .= ($self->{CCFLAGS} ? ' ' : '').'-Wno-error=implicit-function-declaration';
+
+    $self->SUPER::cflags(@_);
+}
+
 1;

commit 22651938f8a9a4ee66028a13ef97c045d0f73556

fix loss of $Config{ccflags} on darwin
a07d95112 broke darwin builds by losing the default for CCFLAGS.

If CCFLAGS isn't provided it is defaulted from $Config{ccflags},
but the the commit above always supplies CCFLAGS and doesn't
include $Config{ccflags}, so any C compiler flags generated by
Configure or hints are discarded.

In core this prevented the -DPERL_DARWIN generated by the hints to
be lost, this prevented the Time::HiRes generating its darwin
clock_nanosleep() emulation, resulting in a test failure.

lib/ExtUtils/MM_Darwin.pm
@@ -2,6 +2,7 @@ package ExtUtils::MM_Darwin;
 
 use strict;
 use warnings;
+use Config;
 
 BEGIN {
     require ExtUtils::MM_Unix;
@@ -55,7 +56,7 @@ Over-ride Apple's automatic setting of -Werror
 sub cflags {
     my $self = shift;
 
-    $self->{CCFLAGS} .= ($self->{CCFLAGS} ? ' ' : '').'-Wno-error=implicit-function-declaration';
+    $self->{CCFLAGS} .= ($self->{CCFLAGS} ? ' ' : "$Config{ccflags} ").'-Wno-error=implicit-function-declaration';
 
     $self->SUPER::cflags(@_);
 }