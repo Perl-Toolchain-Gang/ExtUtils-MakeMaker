2b47d0cccb45408a17944343d8f6df23b6f53f06
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2020-11-19 19:58:01 +0000

    Releasgineering for v7.56

--- lib/ExtUtils/MM_MacOS.pm
+++ lib/ExtUtils/MM_MacOS.pm
@@ -3,7 +3,7 @@ package ExtUtils::MM_MacOS;
 use strict;
 use warnings;
 
-our $VERSION = '7.55_01';
+our $VERSION = '7.56';
 $VERSION =~ tr/_//d;
 
 sub new {
--- lib/ExtUtils/MakeMaker/Config.pm
+++ lib/ExtUtils/MakeMaker/Config.pm
@@ -3,7 +3,7 @@ package ExtUtils::MakeMaker::Config;
 use strict;
 use warnings;
 
-our $VERSION = '7.55_01';
+our $VERSION = '7.56';
 $VERSION =~ tr/_//d;
 
 use Config ();
