bad8e057d2c121fd5b54f4ea0b47af11ed425b6d
Dan Book <grinnz@grinnz.com>
2021-01-31 18:18:44 -0500

    Don't allow hyphens in parsed versions
    Hyphens are not valid in module $VERSION. The preceding regex already does not allow them.

--- lib/ExtUtils/MM_Unix.pm
+++ lib/ExtUtils/MM_Unix.pm
@@ -3020,7 +3020,7 @@ sub parse_version {
       $result = $normal if defined $normal;
     }
     if ( defined $result ) {
-      $result = "undef" unless $result =~ m!^v?[\d_\.\-]+$!
+      $result = "undef" unless $result =~ m!^v?[\d_\.]+$!
                         or eval { version->parse( $result ) };
     }
     $result = "undef" unless defined $result;
