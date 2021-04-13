7115afc36be05f99d891e73a481a017978696da8
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2021-04-13 18:59:37 +0100

    Releasgineering for v7.62

--- lib/ExtUtils/MM_Darwin.pm
+++ lib/ExtUtils/MM_Darwin.pm
@@ -8,7 +8,7 @@ BEGIN {
     our @ISA = qw( ExtUtils::MM_Unix );
 }
 
-our $VERSION = '7.61_01';
+our $VERSION = '7.62';
 $VERSION =~ tr/_//d;
 
 
--- lib/ExtUtils/MM_VMS.pm
+++ lib/ExtUtils/MM_VMS.pm
@@ -16,7 +16,7 @@ BEGIN {
 
 use File::Basename;
 
-our $VERSION = '7.61_01';
+our $VERSION = '7.62';
 $VERSION =~ tr/_//d;
 
 require ExtUtils::MM_Any;
